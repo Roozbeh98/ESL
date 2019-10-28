475b586737a04a65596e77c84e0bea42762e0419
roozbeh shamekhi <roozbehshamekhi@hotmail.com>
2019-10-29 02:13:07 +0330

    ReMatch

---
+++ ESL.Web/Areas/Dashboard/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ESL.DataLayer.Domain;
+
+namespace ESL.Web.Areas.Dashboard.Controllers
+{
+    public class AccountController : Controller
+    {
+        ESLEntities db = new ESLEntities();
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            //if (User.Identity.IsAuthenticated)
+            //{
+            //    return RedirectToAction("Dashboard", "Dashboard");
+
+            //}
+            return View();
+        }
+    }
+}
+
+
+
+
+
+
