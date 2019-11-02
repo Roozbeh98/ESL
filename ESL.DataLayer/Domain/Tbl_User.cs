1f1f78bd83aafbb79f4b7e275242bc18be4232f4
roozbeh shamekhi <roozbehshamekhi@hotmail.com>
2019-11-02 06:15:30 +0330

    new commit

--- ESL.DataLayer/Domain/Tbl_User.cs
+++ ESL.DataLayer/Domain/Tbl_User.cs
@@ -41,6 +41,7 @@ namespace ESL.DataLayer.Domain
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Tbl_News> Tbl_News { get; set; }
+        public virtual Tbl_Role Tbl_Role { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Tbl_UserExam> Tbl_UserExam { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
