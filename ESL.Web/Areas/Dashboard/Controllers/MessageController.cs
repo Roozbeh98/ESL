723cfc7136a945e2b51af66f9ce8a4ec4aa47a0a
roozbeh shamekhi <roozbehshamekhi@hotmail.com>
2019-11-03 04:19:24 +0330

    aside

--- ESL.Web/Areas/Dashboard/Controllers/MessageController.cs
+++ ESL.Web/Areas/Dashboard/Controllers/MessageController.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ESL.Web.Models.Attribute;
 
 namespace ESL.Web.Areas.Dashboard.Controllers
 {
-    [Authorize]
-    public class DashboardController : Controller
+    public class MessageController : Controller
     {
+        // GET: Dashboard/Message
         public ActionResult Index()
         {
             return View();
