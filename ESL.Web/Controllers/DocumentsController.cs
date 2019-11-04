cc8ae59fc2d44d72a6a75a3ce1bdcd4cd1047ce7
roozbeh shamekhi <roozbehshamekhi@hotmail.com>
2019-11-05 00:04:09 +0330

    new marge

---
+++ ESL.Web/Controllers/DocumentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ESL.Web.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        // GET: Documents
+        public ActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
